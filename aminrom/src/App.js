c54c37c11e30397dfa377de00660df70bd755a6e
amineromdhane77 <amineromdhane77@gmail.com>
2023-04-04 10:17:28 +0100

    first commit

---
+++ aminrom/src/App.js
@@ -0,0 +1,16 @@
+
+import Headerr from './composent/Headerr';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formilo from './composent/Formilo';
+
+function App() {
+  return (
+    <div>
+      <Headerr/>
+      <Formilo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ aminrom/src/composent/Headerr.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+
+
+import Carousel from 'react-bootstrap/Carousel';
+import i1 from "./image/1.jpg";
+import i2 from "./image/2.jpg";
+import i3 from "./image/2.jpg";
+
+
+
+function Headerr(){
+    return ( 
+    <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={i1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={i2}
+            alt="Second slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={i3}
+            alt="Third slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+
+}
+export default Headerr;
