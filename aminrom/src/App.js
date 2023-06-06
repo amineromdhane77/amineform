b51471eabd3324d1f4228124934a6ba0ecdd5a8e
amineromdhane77 <124260183+amineromdhane77@users.noreply.github.com>
2023-06-06 16:26:17 +0100

    Update App.js

--- aminrom/src/App.js
+++ aminrom/src/App.js
@@ -1,12 +1,14 @@
 
-import Headerr from './composent/Headerr';
-import './App.css';
+{/* importation de stylesheet de boostrap */}
 import 'bootstrap/dist/css/bootstrap.min.css';
+{/* importation de nos composants */}
+import Headerr from './composent/Headerr';
 import Formilo from './composent/Formilo';
 
 function App() {
   return (
     <div>
+    {/* appelation de nos composants*/}
       <Headerr/>
       <Formilo/>
     </div>
